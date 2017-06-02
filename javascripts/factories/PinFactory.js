028582e44bbcd9610abd0408266b43ccbfd3bb63
ellisthomas <ellisthomas80@gmail.com>
2017-06-01 19:15:56 -0500

    address merge conflict

--- javascripts/factories/PinFactory.js
+++ javascripts/factories/PinFactory.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-app.factory("PinFactory",function($q ,$http,FIREBASE_CONFIG){
-	let getPinsList = (boardId) =>{
-		let pins = [];
-		return new $q ((resolve,reject)=>{
-			$http.get(`${FIREBASE_CONFIG.databaseURL}/pins.json?orderBy="boardId"&equalTo="${boardId}"`)
-			.then((fbItems)=>{
-				let pinsCollection = fbItems.data;
-				console.log("fbItems.data",fbItems.data);
-				if (pinsCollection !== null){
-					Object.keys(pinsCollection).forEach((key) => {
-                            pinsCollection[key].id = key;
-                            pins.push(pinsCollection[key]);
-                        });
-				}
-				resolve(fbIems.data);
-				console.log("pins in getPinsList",pins);
-			})
-			.catch((error)=>{
-				 reject(error);
-				console.log("error in getPinsList ",error);
-			});
-		});
-	};
-
-
-	let postNewPin = (newPin) => {
-=======
 app.factory("PinFactory", function($http, $q, FIREBASE_CONFIG) {
 
 let getPinList = () => {
@@ -50,7 +22,6 @@ let getPinList = () => {
 
 
 	let deletz = (pinId) => {
->>>>>>> 3f06c419a20005fec444cfc3b0da0f3754428976
         return $q((resolve, reject) => {
             $http.delete(`${FIREBASE_CONFIG.databaseURL}/pins/${pinId}.json`)
                 .then((resultz) => {
@@ -72,11 +43,7 @@ let getPinList = () => {
                 })
                 .catch((error) => {
                     reject(error);
-<<<<<<< HEAD
-                    console.log("error in delete pins",error);
-=======
                     console.log("error in postNewPin",error);
->>>>>>> 3f06c419a20005fec444cfc3b0da0f3754428976
                 });
         });
     };
