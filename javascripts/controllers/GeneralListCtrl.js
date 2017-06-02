b3d4c7627d1dd810f3d7682f683d1810f54a91e9
ellisthomas <ellisthomas80@gmail.com>
2017-06-01 19:32:36 -0500

    errors

--- javascripts/controllers/GeneralListCtrl.js
+++ javascripts/controllers/GeneralListCtrl.js
@@ -1,15 +1,9 @@
 app.controller("GeneralListCtrl", function($rootScope, $scope, PinFactory) {
 	$scope.pin = {};
-
-<<<<<<< HEAD
-	let getPins =()=>{
-		PinFactory.getPinsList()
-		.then((pinz)=>{
-=======
+	
 let getAllPins = () => {
 		PinFactory.getPinList()
 		.then((pinz) => {
->>>>>>> 3f06c419a20005fec444cfc3b0da0f3754428976
 			$scope.pins = pinz;
 		}).catch((error)=>{
        console.log("error in getPins", error);
