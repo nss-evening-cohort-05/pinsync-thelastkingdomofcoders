4bea9ee56bf0a07990cba2e2a18b8235998aa988
bryanhmiller <bryankalos@hotmail.com>
2017-06-03 07:37:21 -0500

    Created EditPinCtrl.js and added related partial and route to AppConfig, also created getSinglePin function in PinFactory.js, currently not wired up

---
+++ javascripts/controllers/EditPinCtrl.js
@@ -0,0 +1,20 @@
+app.controller("EdiPintCtrl", function($location, $routeParams, $scope, PinFactory) {
+  $scope.address = {};
+
+  PinFactory.getSinglePin($routeParams.id).then((results) => {
+    $scope.pin = results.data;
+    console.log("getSinglePin $scope.newPin", $scope.pin);
+  }).catch((error) => {
+  	console.log("getSinglePin error", error);
+  });
+
+  $scope.postNewPin = () => {
+  	PinFactory.editItem($scope.pin).then(() => {
+// Need to decide if we return to board or list view
+  		// $location.url("/list");
+  	}).catch((error) => {
+  		console.log("editItem error", error);
+  	});
+  };
+
+});
--- javascripts/factories/PinFactory.js
+++ javascripts/factories/PinFactory.js
@@ -1,25 +1,37 @@
 app.factory("PinFactory", function($http, $q, FIREBASE_CONFIG) {
 
-let getPinList = () => {
-    let pinz = [];
-    return $q((resolve, reject) => {
-        $http.get(`${FIREBASE_CONFIG.databaseURL}/pins.json`)
-            .then((fbItems) => {
-                var pinCollection = fbItems.data;
-                if (pinCollection !== null) {
-                    Object.keys(pinCollection).forEach((key) => {
-                        pinCollection[key].id = key;
-                        pinz.push(pinCollection[key]);
-                    });
-                }
-                resolve(pinz);
-            }).catch((error) => {
-                reject(error);
-
+    let getPinList = () => {
+        let pinz = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/pins.json`)
+                .then((fbItems) => {
+                    var pinCollection = fbItems.data;
+                    if (pinCollection !== null) {
+                        Object.keys(pinCollection).forEach((key) => {
+                            pinCollection[key].id = key;
+                            pinz.push(pinCollection[key]);
+                        });
+                    }
+                    resolve(pinz);
+                }).catch((error) => {
+                    reject(error);
             });
-    });
-};
+        });
+    };
 
+
+    let getSinglePin = (id) => {
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/pins/${id}.json`)
+                .then((resultz) => {
+                    resultz.data.id = id;
+                    resolve(resultz);
+                }).catch((error) => {
+                    reject(error);
+                    console.log("getSingleItem error", error);
+            });
+        });
+    };
 
 	let deletz = (pinId) => {
         return $q((resolve, reject) => {
@@ -50,6 +62,6 @@ let getPinList = () => {
 
 
 
-    return {getPinList: getPinList ,deletz:deletz ,postNewPin:postNewPin};
+    return {getPinList: getPinList , getSinglePin:getSinglePin ,deletz:deletz ,postNewPin:postNewPin};
 
 });
