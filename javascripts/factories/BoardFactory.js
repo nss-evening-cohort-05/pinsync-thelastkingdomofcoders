902135d6aee787488e5d7bfa05e5b643ed459956
ellisthomas <ellisthomas80@gmail.com>
2017-06-04 22:21:34 -0500

    delete board now working

--- javascripts/factories/BoardFactory.js
+++ javascripts/factories/BoardFactory.js
@@ -1,42 +1,55 @@
-app.factory("BoardFactory", function($http, $q, FIREBASE_CONFIG){
-	console.log("inside the BoardFactory app.factory");
+app.factory("BoardFactory", function($http, $q, FIREBASE_CONFIG) {
+    console.log("inside the BoardFactory app.factory");
+
+    let displayUserBoards = (userId) => {
+        let boardz = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/boards.json?orderBy="uid"&equalTo="${userId}"`)
+                .then((fbItems) => {
+                    var boardsCollection = fbItems.data;
+                    if (boardsCollection !== null) {
+                        Object.keys(boardsCollection).forEach((key) => {
+                            boardsCollection[key].id = key;
+                            boardz.push(boardsCollection[key]);
+                        });
+                    }
+                    resolve(boardz);
+                }).catch((error) => {
+                    console.log("error in viewBoards", error);
+                });
+
+        });
+    };
+
+    let postNewBoard = (newBoard) => {
+        return $q((resolve, reject) => {
+            $http.post(`${FIREBASE_CONFIG.databaseURL}/boards.json`, JSON.stringify(newBoard))
+                .then((newBoardResults) => {
+                    resolve(newBoardResults);
+                    console.log("newBoardResults", newBoardResults);
+                }).catch((error) => {
+                    reject("postNewBoard error", error);
+                });
+        });
+
+    };
   
-  let displayUserBoards = (userId) => {
-    let boardz = [];
-    return $q((resolve, reject)=>{
-      $http.get(`${FIREBASE_CONFIG.databaseURL}/boards.json?orderBy="uid"&equalTo="${userId}"`)
-    .then((fbItems)=>{
-      var boardsCollection = fbItems.data;
-        if (boardsCollection !== null) {
-          Object.keys(boardsCollection).forEach((key) =>{
-            boardsCollection[key].id=key;
-            boardz.push(boardsCollection[key]);
-          });
-        }
-      resolve(boardz);
-    }).catch((error)=>{
-      console.log("error in viewBoards", error);
-    });
 
-  });
-};
-
-  let postNewBoard = (newBoard) => {
-  	return $q ((resolve, reject) => {
-  		$http.post(`${FIREBASE_CONFIG.databaseURL}/boards.json`, JSON.stringify(newBoard))
-  		.then((newBoardResults)=>{
-  			resolve(newBoardResults);
-        console.log("newBoardResults", newBoardResults);
-  		}).catch((error)=>{
-  			reject("postNewBoard error", error);
-  		});
-	});
+    let deletzBoard = (id) => {
+            return $q((resolve, reject) => {
+                $http.delete(`${FIREBASE_CONFIG.databaseURL}/boards/${id}.json`)
+                    .then((resultz) => {
+                        resolve(resultz);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            });
+        };
 
 
-    
-  };
 
-return {postNewBoard:postNewBoard, displayUserBoards: displayUserBoards};
+    return {postNewBoard: postNewBoard, displayUserBoards: displayUserBoards, deletzBoard: deletzBoard};
 
 
 });
